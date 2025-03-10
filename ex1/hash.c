fbf45d545d242709e1037203abcc3d35f68e4a0e
eric <ericxia168@gmail.com>
2025-03-10 14:44:39 +0100

    ex1

---
+++ ex1/hash.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "hash.h"
+
+#define A ((sqrt(5) - 1) / 2)
+
+unsigned long simple_hash(const char* str){
+    /*convertis une chaıne de caracteres en un indice dans la table de hachage*/
+    unsigned long cle = 0;
+    for (unsigned long i = 0; i < strlen(str); i++){
+        cle += str[i];
+    }
+    double frac_part = cle * A - floor(cle * A);
+    unsigned long res = (unsigned long)(TABLE_SIZE * frac_part);
+    return res;
+}
+
+HashMap* hashmap_create(){
+    /*alloue dynamiquement une table de hachage et initialise ses cases a NULL*/
+    HashMap* res = (HashMap*)malloc(sizeof(HashMap));
+    res->size = TABLE_SIZE;
+    res->table = calloc(res->size, sizeof(HashEntry));
+    return res;
+}
+
+int hashmap_insert(HashMap *map, const char *key, void *value){
+    /*insere un element dans la table de hachage*/
+    unsigned long cle = simple_hash(key);
+    while (map->table[cle].key != NULL && map->table[cle].key != TOMBSTONE) {
+        if (strcmp(map->table[cle].key, key) == 0) {
+            map->table[cle].value = value;
+            return 0;
+        }
+        cle = (cle + 1) % TABLE_SIZE;
+    }
+    map->table[cle].key = strdup(key);
+    map->table[cle].value = value;
+    return 0;
+}
+
+void *hashmap_get(HashMap *map, const char *key){
+    /*recupere un element a partir de sa cle*/
+    unsigned long cle = simple_hash(key);
+    while (map->table[cle].key != NULL) {
+        if (map->table[cle].key != TOMBSTONE && strcmp(map->table[cle].key, key) == 0) {
+            return map->table[cle].value;
+        }
+        cle = (cle + 1) % TABLE_SIZE;
+    }
+    return NULL;
+}
+
+int hashmap_remove(HashMap *map, const char *key){
+    /*supprime un element de la table de hachage tout en assurant la continuite du sondage lineaire*/
+    unsigned long cle = simple_hash(key);
+    while (map->table[cle].key != NULL) {
+        if (map->table[cle].key != TOMBSTONE && strcmp(map->table[cle].key, key) == 0) {
+            free(map->table[cle].key);
+            map->table[cle].key = TOMBSTONE;
+            map->table[cle].value = TOMBSTONE;
+            return 0;
+        }
+        cle = (cle + 1) % TABLE_SIZE;
+    }
+    return -1;
+}
+
+void hashmap_destroy(HashMap *map){
+    /*libere toute la memoire allouee a la table de hachage*/
+    for (int i = 0; i < TABLE_SIZE; i++){
+        if (map->table[i].key != NULL && map->table[i].key != TOMBSTONE) {
+            free(map->table[i].key);
+        }
+    }
+    free(map->table);
+    free(map);
+}
---
+++ ex1/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <number_of_elements>\n", argv[0]);
+        return 1;
+    }
+
+    int num_elements = atoi(argv[1]);
+    if (num_elements <= 0) {
+        fprintf(stderr, "Le nombre d'element doit etre positif\n");
+        return 1;
+    }
+
+    // Créer une nouvelle table de hachage
+    HashMap *map = hashmap_create();
+
+    // Insérer des éléments dans la table de hachage 
+    for (int i = 1; i <= num_elements; i++) {
+        char key[20];
+        char *value = malloc(20 * sizeof(char));
+        snprintf(key, sizeof(key), "key%d", i);
+        snprintf(value, 20, "value%d", i);
+        hashmap_insert(map, key, value);
+    }
+
+    // Récupérer des éléments de la table de hachage
+    for (int i = 1; i <= num_elements; i++) {
+        char key[20];
+        snprintf(key, sizeof(key), "key%d", i);
+        printf("%s: %s\n", key, (char *)hashmap_get(map, key));
+    }
+
+    // Supprimer un élément de la table de hachage
+    char key_to_remove[20];
+    snprintf(key_to_remove, sizeof(key_to_remove), "key%d", num_elements);
+    hashmap_remove(map, key_to_remove);
+
+    // Essayer de récupérer l'élément supprimé
+    printf("%s: %s\n", key_to_remove, (char *)hashmap_get(map, key_to_remove));
+
+    // Détruire la table de hachage
+    hashmap_destroy(map);
+
+    return 0;
+}
