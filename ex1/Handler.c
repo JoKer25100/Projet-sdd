46aa28dd620cdc540d963910fa90359960193031
eric <ericxia168@gmail.com>
2025-03-10 18:03:50 +0100

    ex2

---
+++ ex1/Handler.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "hash.h"
+#include "Handler.h"
+
+MemoryHandler *memory_init(int size){
+    MemoryHandler *res = (MemoryHandler*)malloc(sizeof(MemoryHandler));
+    res->total_size = size;
+    res->memory = (void**)malloc(size * sizeof(void*));
+    res->free_list = (Segment*)malloc(sizeof(Segment));
+    res->free_list->start = 0;
+    res->free_list->size = size;
+    res->free_list->next = NULL;
+    res->allocated = hashmap_create();
+    return res;
+}
+
+Segment *find_free_segment(MemoryHandler* handler, int start, int size, Segment** prev){
+    Segment *current = handler->free_list;
+    *prev = NULL;
+    while (current != NULL){
+        if (current->start <= start && (current->start + current->size) >= (start + size)){
+            return current;
+        }
+        if (current->start > start){
+            return NULL;
+        }
+        *prev = current;
+        current = current->next;
+    }
+    return NULL;
+}
+
+Segment *creer_segment(int start, int size){
+    Segment *new_segment = (Segment*)malloc(sizeof(Segment));
+    new_segment->start = start;
+    new_segment->size = size;
+    new_segment->next = NULL;
+    return new_segment;
+}
+
+
+int create_segment(MemoryHandler *handler, const char *name, int start, int size) {
+    Segment *prev = NULL;
+    Segment *current = find_free_segment(handler, start, size, &prev);
+    if (current == NULL) {
+        return -1;
+    }
+
+    // Si le segment trouvé correspond exactement à la taille demandée
+    if (current->start == start && current->size == size) {
+        if (prev == NULL) {
+            handler->free_list = current->next;
+        } else {
+            prev->next = current->next;
+        }
+        free(current);
+    } else {
+        // Si le segment trouvé est plus grand que la taille demandée
+        if (current->start == start) {
+            current->start += size;
+            current->size -= size;
+        } else if (current->start + current->size == start + size) {
+            current->size -= size;
+        } else {
+            Segment *new_segment = creer_segment(start + size, current->start + current->size - (start + size));
+            new_segment->next = current->next;
+            current->next = new_segment;
+            current->size = start - current->start;
+        }
+    }
+
+    Segment *allocated_segment = creer_segment(start, size);
+    hashmap_insert(handler->allocated, name, allocated_segment);
+    return 0;
+}
